8366a75845c9e3e42e9aa926901214f2776e1194
israel <ic850536@gmail.com>
2021-04-04 19:34:57 +0300

    change demo2

--- demo2.py
+++ demo2.py
@@ -1,1 +1,12 @@
-print("yo")
+
+
+def show_love(num):
+    if num%2 == 0:
+        print("is even")
+    else:
+        print("is not even")
+
+
+if __name__ == "__main__":
+    input1 = int(input("write your number"))
+    show_love(input1)
